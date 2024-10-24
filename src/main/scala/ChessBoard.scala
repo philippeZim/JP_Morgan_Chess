3f8f93c4c9b8639de61d818e5ffafe12a71ba0a8
Philippe <philippe.zim22@gmail.com>
2024-10-24 21:11:56 +0200

    reset to working version4

--- src/main/scala/ChessBoard.scala
+++ src/main/scala/ChessBoard.scala
@@ -26,8 +26,31 @@ object ChessBoard {
     }
 
     def getDefaultBoard: Vector[Vector[Piece]] = {
-        val defaultFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-        fenToBoard(defaultFEN)
+        val p = Piece(PieceType.PAWN, Color.BLACK);
+        val r = Piece(PieceType.ROOK, Color.BLACK);
+        val n = Piece(PieceType.KNIGHT, Color.BLACK);
+        val b = Piece(PieceType.BISHOP, Color.BLACK);
+        val q = Piece(PieceType.QUEEN, Color.BLACK);
+        val k = Piece(PieceType.KING, Color.BLACK);
+
+        val P = Piece(PieceType.PAWN, Color.WHITE);
+        val R = Piece(PieceType.ROOK, Color.WHITE);
+        val N = Piece(PieceType.KNIGHT, Color.WHITE);
+        val B = Piece(PieceType.BISHOP, Color.WHITE);
+        val Q = Piece(PieceType.QUEEN, Color.WHITE);
+        val K = Piece(PieceType.KING, Color.WHITE);
+        val point = Piece(PieceType.EMPTY, Color.EMPTY);
+        val board: Vector[Vector[Piece]] = Vector(
+            Vector(r, n, b, q, k, b, n, r),
+            Vector(p, p, p, p, p, p, p, p),
+            Vector(point, point, point, point, point, point, point, point),
+            Vector(point, point, point, point, point, point, point, point),
+            Vector(point, point, point, point, point, point, point, point),
+            Vector(point, point, point, point, point, point, point, point),
+            Vector(P, P, P, P, P, P, P, P),
+            Vector(R, N, B, Q, K, B, N, R)
+        )
+        board
     }
 
     def fenToBoard(fen: String): Vector[Vector[Piece]] = {
