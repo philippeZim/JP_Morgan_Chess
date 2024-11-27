e8c23881d8780ba18cdf7eea4abe11fd368d38f1
Philippe <philippe.zim22@gmail.com>
2024-11-27 17:41:30 +0100

    Remi logic

--- src/main/scala/Model/Remis.scala
+++ src/main/scala/Model/Remis.scala
@@ -1,5 +1,7 @@
 package Model
 
+import scala.annotation.tailrec
+
 object Remis {
 
 
@@ -16,8 +18,90 @@ object Remis {
         }
         false
     }
+    
+    
+
+    def isMaterial(fen: String): Boolean = {
+        
+        @tailrec
+        def sub1(li: List[Char], hadBishop: Boolean, hasWhiteExtra: Option[Boolean]): Boolean = {
+            li match {
+                case Nil => false;
+                case h::t => h match {
+                    case 'p' => false
+                    case 'P' => false
+                    case 'r' => false
+                    case 'R' => false
+                    case 'q' => false
+                    case 'Q' => false
+                    case 'n' =>
+                        hasWhiteExtra match {
+                            case Some(a) =>
+                                if (a) {
+                                    false
+                                } else {
+                                    sub1(t, hadBishop, Some(false));
+                                }
+                            case None => sub1(t, hadBishop, Some(false));
+                        }
+                        
+                    case 'N' =>
+                        hasWhiteExtra match {
+                            case Some(a) =>
+                                if (!a) {
+                                    false
+                                } else {
+                                    sub1(t, hadBishop, Some(true));
+                                }
+                            case None => sub1(t, hadBishop, Some(true));
+                        }
+                        
+                    case 'b' =>
+                        hasWhiteExtra match {
+                            case Some(a) =>
+                                if (a) {
+                                    false
+                                } else {
+                                    if (hadBishop) {
+                                        false
+                                    } else {
+                                        sub1(t, true, Some(false))
+                                    }
+                                }
+                            case None =>
+                                if (hadBishop) {
+                                    false
+                                } else {
+                                    sub1(t, true, Some(false))
+                                }
+                        }
+                    case 'B' =>
+                        hasWhiteExtra match {
+                            case Some(a) =>
+                                if (!a) {
+                                    false
+                                } else {
+                                    if (hadBishop) {
+                                        false
+                                    } else {
+                                        sub1(t, true, Some(true))
+                                    }
+                                }
+                            case None =>
+                                if (hadBishop) {
+                                    false
+                                } else {
+                                    sub1(t, true, Some(true))
+                                }
+                        }
+                    case _ => sub1(t, hadBishop, hasWhiteExtra)
+                }
+            }
+        }
+        sub1(fen.split(" ")(0).toList, false, None);
+    }
 
     def isRemis(fen: String, legalMoves: List[(Int, Int)]) : Boolean = {
-        false
+        isPatt(fen, legalMoves) || isMaterial(fen)
     }
 }
