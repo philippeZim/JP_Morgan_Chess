073b44dec9b4a802bfc49e67d2391a0d86d53f2e
JakobPreiss <jakob.preiss.htwg@gmail.com>
2024-11-29 16:40:46 +0100

    own Board Monad

--- src/main/scala/Model/BoardMonad.scala
+++ src/main/scala/Model/BoardMonad.scala
@@ -1,5 +1,4 @@
-import Model.{ChessBoard, Color, Piece, PieceType}
-
+package Model
 
 
 class BoardMonad[T](board: Vector[Piece]) {
@@ -24,23 +23,6 @@ class BoardMonad[T](board: Vector[Piece]) {
 }
 
 def piecePositions(board: Vector[Piece], piece: Piece): List[Int] = {
-    def sub(board: List[Piece], accumulator: List[Int], index: Int): List[Int] = {
-        board match {
-            case Nil => accumulator
-            case h :: t => {
-                if (h == piece) {
-                    sub(t, index :: accumulator, index + 1);
-                } else {
-                    sub(t, accumulator, index + 1);
-                }
-            }
-        }
-    }
-
-    sub(board.toList, List(), 0);
-}
-
-def piecePositionsMonad(board: Vector[Piece], piece: Piece): List[Int] = {
     val bm = BoardMonad(board)
     var index = -1
     val bm_index = bm.map(e =>
@@ -51,11 +33,8 @@ def piecePositionsMonad(board: Vector[Piece], piece: Piece): List[Int] = {
         (e, i) => e match {
             case None => false
             case Some(a) if a.pieceType == piece.pieceType && a.color == piece.color => true
-            case _ => false
         }
     )
     correct_pieces_with_index.map((e, i) => i).toList
 }
 
-println(piecePositions(ChessBoard.getDefaultBoard(), new Piece(PieceType.KNIGHT, Color.WHITE)))
-println(piecePositionsMonad(ChessBoard.getDefaultBoard(), new Piece(PieceType.KNIGHT, Color.WHITE)))
