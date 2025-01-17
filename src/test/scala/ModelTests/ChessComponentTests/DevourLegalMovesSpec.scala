e3aa6cb7ac4a438aec7b0cd3646881459b885032
Philippe <philippe.zim22@gmail.com>
2025-01-17 11:45:11 +0100

    Testing

--- src/test/scala/ModelTests/ChessComponentTests/DevourLegalMovesSpec.scala
+++
@@ -1,8 +0,0 @@
-package ModelTests.ChessComponentTests
-
-import org.scalatest.wordspec.AnyWordSpec
-import org.scalatest.matchers.should.Matchers.*
-
-class DevourLegalMovesSpec extends AnyWordSpec {
-    
-}
